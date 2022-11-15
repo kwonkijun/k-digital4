ab9e982eed98901e97c414f9ba52ac5ce04bc5c2
kwonkijun <kkj6369@naver.com>
2022-11-15 10:56:00 +0900

    'a'

--- 07.GUI/01.로그인.py
+++ 07.GUI/01.로그인.py
@@ -10,6 +10,17 @@ class MainDialog(QDialog):
         QDialog.__init__(self, None)
         uic.loadUi(UI_PATH, self)
 
+        # 버튼 클릭 이벤트 주는 방법
+        # self.객체이름.clicked.connect(self.실행할함수이름)
+        self.login_btn.clicked.connect(self.login_start)
+    
+    def login_start(self):
+        input_id = self.id.text()
+        input_pw = self.pw.text()
+        print(f'입력한 아이디는 : {input_id}')
+        print(f'입력한 패스워드는 : {input_pw}')
+
+
 QApplication.setStyle('fusion')
 app = QApplication(sys.argv)
 main_dialog = MainDialog()
